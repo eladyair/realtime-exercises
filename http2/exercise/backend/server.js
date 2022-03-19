188443b136d783915bcad64fba960921dddc5b27
Elad Yair <eladyair@gmail.com>
2022-03-19 23:08:59 +0200

    Programming the code to use http2 push for realtime use case between server and client

--- http2/exercise/backend/server.js
+++ http2/exercise/backend/server.js
@@ -11,9 +11,9 @@ const msg = new nanobuffer(50);
 const getMsgs = () => Array.from(msg).reverse();
 
 msg.push({
-  user: "brian",
-  text: "hi",
-  time: Date.now(),
+    user: "brian",
+    text: "hi",
+    time: Date.now(),
 });
 
 // the two commands you'll have to run in the root directory of the project are
@@ -25,8 +25,8 @@ msg.push({
 // http2 only works over HTTPS
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const server = http2.createSecureServer({
-  cert: fs.readFileSync(path.join(__dirname, "/../server.crt")),
-  key: fs.readFileSync(path.join(__dirname, "/../key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "/../server.crt")),
+    key: fs.readFileSync(path.join(__dirname, "/../key.pem")),
 });
 
 /*
@@ -35,36 +35,64 @@ const server = http2.createSecureServer({
  *
  */
 
-server.on("request", async (req, res) => {
-  const path = req.headers[":path"];
-  const method = req.headers[":method"];
+server.on("stream", (stream, headers) => {
+    const path = headers[":path"];
+    const method = headers[":method"];
 
-  if (path !== "/msgs") {
-    // handle the static assets
-    return handler(req, res, {
-      public: "./frontend",
-    });
-  } else if (method === "POST") {
-    // get data out of post
-    const buffers = [];
-    for await (const chunk of req) {
-      buffers.push(chunk);
+    if (path === "/msgs" && method === "GET") {
+        console.log("Connected a stream " + stream.id);
+        stream.respond({
+            ":status": 200,
+            "content-type": "text/plain; charset=utf-8",
+        });
+
+        // write the first response
+        stream.write(JSON.stringify({ msg: getMsgs() }));
+        connections.push(stream);
+
+        stream.on("close", () => {
+            console.log("Disconnected a stream " + stream.id);
+            connections = connections.filter((c) => c !== stream);
+        });
     }
-    const data = Buffer.concat(buffers).toString();
-    const { user, text } = JSON.parse(data);
+});
 
-    /*
-     *
-     * some code goes here
-     *
-     */
-  }
+server.on("request", async (req, res) => {
+    const path = req.headers[":path"];
+    const method = req.headers[":method"];
+
+    if (path !== "/msgs") {
+        // handle the static assets
+        return handler(req, res, {
+            public: "./frontend",
+        });
+    } else if (method === "POST") {
+        // get data out of post
+        const buffers = [];
+        for await (const chunk of req) {
+            buffers.push(chunk);
+        }
+        const data = Buffer.concat(buffers).toString();
+        const { user, text } = JSON.parse(data);
+
+        msg.push({
+            user,
+            text,
+            time: Date.now(),
+        });
+
+        res.end();
+
+        connections.forEach((stream) =>
+            stream.write(JSON.stringify({ msg: getMsgs() }))
+        );
+    }
 });
 
 // start listening
 const port = process.env.PORT || 8080;
 server.listen(port, () =>
-  console.log(
-    `Server running at https://localhost:${port} - make sure you're on httpS, not http`
-  )
+    console.log(
+        `Server running at https://localhost:${port} - make sure you're on httpS, not http`
+    )
 );
--- http2/exercise/frontend/http2-chat.js
+++ http2/exercise/frontend/http2-chat.js
@@ -6,47 +6,81 @@ const presence = document.getElementById("presence-indicator");
 let allChat = [];
 
 chat.addEventListener("submit", function (e) {
-  e.preventDefault();
-  postNewMsg(chat.elements.user.value, chat.elements.text.value);
-  chat.elements.text.value = "";
+    e.preventDefault();
+    postNewMsg(chat.elements.user.value, chat.elements.text.value);
+    chat.elements.text.value = "";
 });
 
 async function postNewMsg(user, text) {
-  const data = {
-    user,
-    text,
-  };
+    const data = {
+        user,
+        text,
+    };
 
-  // request options
-  const options = {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
+    // request options
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
 
-  // send POST request
-  // we're not sending any json back, but we could
-  await fetch("/msgs", options);
+    // send POST request
+    // we're not sending any json back, but we could
+    await fetch("/msgs", options);
 }
 
 async function getNewMsgs() {
-  /*
-   *
-   * code goes here
-   *
-   */
+    let reader;
+    const utf8Decoder = new TextDecoder("utf-8");
+
+    try {
+        const res = await fetch("/msgs");
+        reader = res.body.getReader();
+    } catch (error) {
+        console.log("connection error: ", error);
+    }
+
+    presence.innerText = "🟢";
+
+    let readerResponse;
+    let done;
+    do {
+        try {
+            readerResponse = await reader.read();
+        } catch (error) {
+            console.error("reader error: ", error);
+            presence.innerText = "🔴";
+            return;
+        }
+        const chunk = utf8Decoder.decode(readerResponse.value, {
+            stream: true,
+        });
+
+        done = readerResponse.done;
+
+        if (chunk) {
+            try {
+                const json = JSON.parse(chunk);
+                allChat.push(json.msg);
+                render();
+            } catch (error) {
+                console.error("parse error: ", error);
+            }
+        }
+    } while (!done);
+    presence.innerText = "🔴";
 }
 
 function render() {
-  const html = allChat.map(({ user, text, time, id }) =>
-    template(user, text, time, id)
-  );
-  msgs.innerHTML = html.join("\n");
+    const html = allChat.map(({ user, text, time, id }) =>
+        template(user, text, time, id)
+    );
+    msgs.innerHTML = html.join("\n");
 }
 
 const template = (user, msg) =>
-  `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
+    `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
 
 getNewMsgs();

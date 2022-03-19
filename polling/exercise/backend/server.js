e562695ee67c2e294587cdecf432dd8f675582be
Elad Yair <eladyair@gmail.com>
2022-03-19 18:09:02 +0200

    Programming the code to handle polling from the server

--- polling/exercise/backend/server.js
+++ polling/exercise/backend/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import bodyParser from "body-parser";
 import nanobuffer from "nanobuffer";
 import morgan from "morgan";
 
@@ -9,7 +8,7 @@ const getMsgs = () => Array.from(msg).reverse();
 
 // feel free to take out, this just seeds the server with at least one message
 msg.push({
-  user: "brian",
+  user: "elad",
   text: "hi",
   time: Date.now(),
 });
@@ -17,17 +16,29 @@ msg.push({
 // get express ready to run
 const app = express();
 app.use(morgan("dev"));
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(express.static("frontend"));
 
 app.get("/poll", function (req, res) {
   // use getMsgs to get messages to send back
-  // write code here
+  return res.json({
+    msgs: getMsgs(),
+  });
 });
 
 app.post("/poll", function (req, res) {
   // add a new message to the server
-  // write code here
+  const { user, text } = req.body;
+
+  msg.push({
+    user,
+    text,
+    time: Date.now(),
+  });
+
+  res.json({
+    status: "ok",
+  });
 });
 
 // start the server
--- polling/exercise/frontend/polling-chat.js
+++ polling/exercise/frontend/polling-chat.js
@@ -14,28 +14,77 @@ chat.addEventListener("submit", function (e) {
   chat.elements.text.value = "";
 });
 
-async function postNewMsg(user, text) {
+const postNewMsg = async (user, text) => {
   // post to /poll a new message
-  // write code here
-}
+  const data = {
+    user,
+    text,
+  };
 
-async function getNewMsgs() {
-  // poll the server
-  // write code here
-}
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
-function render() {
+  const res = await fetch("/poll", options);
+  const json = await res.json();
+};
+
+const render = () => {
   // as long as allChat is holding all current messages, this will render them
   // into the ui. yes, it's inefficent. yes, it's fine for this example
   const html = allChat.map(({ user, text, time, id }) =>
     template(user, text, time, id)
   );
+
   msgs.innerHTML = html.join("\n");
-}
+};
+
+const getNewMsgs = async () => {
+  // poll the server
+  let json;
+  try {
+    const res = await fetch("/poll");
+    json = await res.json();
+
+    if (res.status >= 400) {
+      throw new Error("request failed: ", res.status);
+    }
+
+    allChat = json.msgs;
+    render();
+
+    failedTries = 0;
+  } catch (e) {
+    // back off could would go here
+    console.error("polling error", e);
+    failedTries++;
+  }
+  // allChat = json.msgs;
+  // render();
+  // setTimeout(getNewMsgs, INTERVAL);
+};
 
 // given a user and a msg, it returns an HTML string to render to the UI
 const template = (user, msg) =>
   `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
 
 // make the first request
-getNewMsgs();
+//getNewMsgs();
+
+const BACKOFF = 5000;
+let timeToMakeNextRequest = 0;
+let failedTries = 0;
+const rafTimer = async (time) => {
+  if (timeToMakeNextRequest <= time) {
+    await getNewMsgs();
+    timeToMakeNextRequest = time + INTERVAL + failedTries * BACKOFF;
+  }
+
+  requestAnimationFrame(rafTimer);
+};
+
+requestAnimationFrame(rafTimer);
